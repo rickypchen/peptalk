f8c68435d9a08d2930710415e9771a6dd682d91b
rickypchen <richard.polin.chen@gmail.com>
2015-03-10 13:55:17 -0700

    add register and log in forms in index page, create user model and migration

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -2,3 +2,12 @@ get '/' do
   erb :index
 end
 
+post '/users/new' do
+  user = User.new(username: params[:username], first_name: params[:first_name], last_name: params[:last_name])
+  user.save
+  redirect '/'
+end
+
+get '/wall' do
+  erb :wall
+end
