e76f743c41fa4cd9d3407aa420d248844bf7a215
rickypchen <richard.polin.chen@gmail.com>
2015-03-11 00:04:33 -0700

    add styles to user profile page

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,5 +1,6 @@
-BANDNAMES = [ "Joy Division", "New Order", "The Smiths", "The Sisters of Mercy", "Wye Oak", "Thao and the Get Down Stay Down",
-          "Siouxsie and the Banshees", "Interpol", "Ted Leo and The Pharmacists", "The Impossibles", "The Promise Ring",
-          "Fugazi" ]
+QUOTES = ["Strive not to be a success, but rather to be of value", "Doubt whom you will, but never yourself", "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference", "You miss 100% of the shots you don’t take", "Eighty percent of success is showing up", "Your time is limited, so don’t waste it living someone else’s life", "Every child is an artist.  The problem is how to remain an artist once he grows up"]
+AUTHORS = ["Albert Einstein", "Christian Nestell Bovee", "Robert Frost", "Wayne Gretzky", "Woody Allen", "Steve Jobs", "Pablo Picasso"]
 
-BANDNAMES.each{ |band_name| Band.create(name: band_name) }
+quotes = Hash[AUTHORS.zip(QUOTES)]
+
+quotes.each{ |author, quote| Quote.create(author: author, quote: quote, user_id: 5) }
