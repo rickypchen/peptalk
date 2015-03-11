d4df4e734f26576a51c1e4116105e99cc8bd6b20
rickypchen <richard.polin.chen@gmail.com>
2015-03-10 17:43:31 -0700

    add user validation and errors

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -3,8 +3,9 @@ get '/' do
 end
 
 post '/users/new' do
-  user = User.new(username: params[:username], first_name: params[:first_name], last_name: params[:last_name])
-  user.save
+  @user = User.new(username: params[:username], first_name: params[:first_name], last_name: params[:last_name])
+  @user.save
+  flash[:error] = @user.errors.full_messages
   redirect '/'
 end
 
--- source/app/helpers/formatting.rb
+++ source/app/helpers/formatting.rb
@@ -2,4 +2,10 @@ helpers do
   def em(text)
     "<em>#{text}</em>"
   end
+
+  def display_errors
+    if flash[:error]
+      flash[:error].to_sentence
+    end
+  end
 end
