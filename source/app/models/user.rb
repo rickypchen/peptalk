ff9bd2f934ce091e81a92304d56ce2707628c85d
rickypchen <richard.polin.chen@gmail.com>
2015-03-10 19:40:36 -0700

    add bcrypt in user model and include gem

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  validates_presence_of :username, :first_name, :last_name
+  validates_presence_of :username, :first_name, :last_name, :password_hash
+
+  validates :username, uniqueness: true
+
+  include BCrypt
+
+    def password
+      @password ||= Password.new(password_hash)
+    end
+
+    def password=(new_password)
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    end
+
 end
