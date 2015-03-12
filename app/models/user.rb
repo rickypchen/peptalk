5e37f6bd40b36c0f6031eb837676cff0d6a40ac5
rickypchen <richard.polin.chen@gmail.com>
2015-03-12 00:35:38 -0700

    set up user many-to-many relationship with other users and add more users in seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_many :quotes
+
+  has_many :actively_following, :class_name => "Motivation", :foreign_key => "user_id"
+  has_many :being_followed, :class_name => "Motivation", :foreign_key => "subscribed_to_id"
+
+   has_many :motivators, :through => :actively_following
+  has_many :followers, :through => :being_followed
+
   validates_presence_of :username, :first_name, :last_name, :password
 
   validates :username, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,22 @@
-QUOTES = ["Strive not to be a success, but rather to be of value", "Doubt whom you will, but never yourself", "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference", "You miss 100% of the shots you don’t take", "Eighty percent of success is showing up", "Your time is limited, so don’t waste it living someone else’s life", "Every child is an artist.  The problem is how to remain an artist once he grows up"]
-AUTHORS = ["Albert Einstein", "Christian Nestell Bovee", "Robert Frost", "Wayne Gretzky", "Woody Allen", "Steve Jobs", "Pablo Picasso"]
+QUOTES = ["Strive not to be a success, but rather to be of value", "Doubt whom you will, but never yourself", "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference", "You miss 100% of the shots you don’t take", "Eighty percent of success is showing up", "Your time is limited, so don’t waste it living someone else’s life", "Every child is an artist.  The problem is how to remain an artist once he grows up", "Go confidently in the direction of your dreams.  Live the life you have imagined", "Life is not measured by the number of breaths we take, but by the moments that take our breath away"]
+AUTHORS = ["Albert Einstein", "Christian Nestell Bovee", "Robert Frost", "Wayne Gretzky", "Woody Allen", "Steve Jobs", "Pablo Picasso", "Henry David Thoreau", "Maya Angelou"]
+
+USERS = [
+  {username: "JeanPoole", first_name: "Jean", last_name: "Poole", password: "password"},
+  {username: "EdwardSnooden", first_name: "Edward", last_name: "Snooden", password: "password"},
+  {username: "AboveWind", first_name: "Above", last_name: "Wind", password: "password"},
+  {username: "franksinatra", first_name: "Frank", last_name: "Sinatra", password: "password"},
+  {username: "inedibleflamingo", first_name: "Flaming", last_name: "Ed", password: "password"},
+  {username: "growthpains", first_name: "Peter", last_name: "Piper", password: "password"},
+  {username: "chucknorris", first_name: "Chuck", last_name: "Norris", password: "password"},
+]
 
 quotes = Hash[AUTHORS.zip(QUOTES)]
 
-quotes.each{ |author, quote| Quote.create(author: author, quote: quote, user_id: 5) }
+quotes.each do |author, quote|
+  Quote.create(author: author, quote: quote, user_id: rand(1..7))
+end
+
+USERS.each do |user|
+  User.create(username: user[:username], first_name: user[:first_name], last_name: user[:last_name], password: user[:password])
+end
